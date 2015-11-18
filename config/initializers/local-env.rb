db51d7b428656ea25b6a937f076befc12f8d05fc
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-11-18 11:52:37 +0100

    Refactors

--- config/initializers/local-env.rb
+++ config/initializers/local-env.rb
@@ -1,8 +1,11 @@
 local_env = YAML.load_file("#{Rails.root}/config/local-env.yml") rescue nil
 
-if local_env
-  local_env = local_env[Rails.env] || local_env
-  local_env.each_pair do |key, value|
-    ENV[key] ||= value
+if local_env && local_env[Rails.env]
+  local_env = local_env[Rails.env]
+
+  if local_env.class == Hash
+    local_env.each do |key, value|
+      ENV[key] = value
+    end
   end
 end
